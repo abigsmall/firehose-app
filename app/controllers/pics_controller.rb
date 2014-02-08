9a9c8b519833af3ffc02378bbe4a7b18177edcd9
abigsmall <abigsmall@gmail.com>
2014-02-08 17:11:07 -0500

    adding the form heheh

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,25 @@ class PicsController < ApplicationController
 	def index
 		@pics = Pic.all
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		# Pic.create(:hour => 1,
+		#			 :emotion => "Stoked",
+		#			 :learned => "HTML and CSS rocks!")
+		Pic.create( pics_params )
+		redirect_to pics_path
+	end
+
+	private
+
+	def pics_params
+		params.require(:pic).permit(
+			:hour, :emotion, :learned
+		)
+	end
+
 end
